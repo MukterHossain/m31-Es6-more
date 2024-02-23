280df21bd39097fdadc0c04aa25822ad25ce7a61
MukterHossain <mukterhossain3075@gmail.com>
2024-02-23 16:36:47 +0600

    added last messege

--- task.js
+++ task.js
@@ -104,8 +104,44 @@ const result = numbers.map(even2)
 //task-5
 const number2 = [33, 50, 79, 78, 90, 101, 30];
 const result2 =number2.filter(number2 => number2 % 10 ===0);
-console.log(result2)
+// console.log(result2)
 
 // or
 const selected = number2.find(number2 => number2 % 10===0);
-console.log(selected)
+// console.log(selected)
+
+
+
+
+
+// task-6
+const instructor =[
+    {name: 'Nodi', age: 28, position: 'Senior'},
+    {name: 'Akil', age: 26, position: 'Junior'},
+    {name: 'Shobuj', age: 30, position: 'Senior'},
+];
+
+const result3 = instructor.map(ins => ins.position == 'Senior');
+console.log(result3)
+
+
+
+
+
+
+// task -7
+const people = [
+    {name: 'Meena', age:20},
+    {name: 'Rina', age:15},
+    {name: 'Suchorita', age:22}
+];
+
+const result4 = people.reduce((a, b) => a+b.age, 0)
+// console.log(result4)
+
+
+
+// const total = people.reduce( (acum, current) => acum + current.age, 0)
+// console.log(total)
+
+
